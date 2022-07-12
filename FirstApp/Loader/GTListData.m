c34bb9846c60dd14ce5ca0d4e496e0bfb8ad92b6
Yang Gao <maxshine_au@outlook.com>
2022-07-12 15:40:28 +1000

    add NSURLSessionTask

--- FirstApp/Loader/GTListData.m
+++ FirstApp/Loader/GTListData.m
@@ -13,7 +13,14 @@
     
     NSString *urlString = @"https://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
     NSURL *listURL = [NSURL URLWithString:urlString];
-    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+    
+    NSURLSession *urlSession = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [urlSession dataTaskWithRequest:listRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"");
+    }];
+    [dataTask resume];
+    NSLog(@"");
 }
 
 @end
