73262719330004fc3d15d85dd19e696996488a5b
Yang Gao <maxshine_au@outlook.com>
2022-07-24 09:00:32 +1000

    add player progress callback

--- FirstApp/VideoController/GTVideoCoverView.m
+++ FirstApp/VideoController/GTVideoCoverView.m
@@ -32,6 +32,7 @@
 - (void) dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     [self.avPlayerItem removeObserver:self forKeyPath:@"status"];
+    [self.avPlayerItem removeObserver:self forKeyPath:@"loadedTimeRanges"];
 }
 
 - (void) tapToPlay {
@@ -40,7 +41,11 @@
     AVAsset *asset = [AVAsset assetWithURL:url];
     self.avPlayerItem = [AVPlayerItem playerItemWithAsset:asset];
     [self.avPlayerItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    [self.avPlayerItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
     self.avPlayer = [AVPlayer playerWithPlayerItem:self.avPlayerItem];
+    [self.avPlayer addPeriodicTimeObserverForInterval:CMTimeMake(10, 1) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+        NSLog(@"Played %f", CMTimeGetSeconds(time));
+    }];
     self.avPlayerLayer = [AVPlayerLayer playerLayerWithPlayer:self.avPlayer ];
     self.avPlayerLayer.frame = self.coverView.bounds;
     [self.coverView.layer addSublayer:self.avPlayerLayer];
@@ -57,15 +62,20 @@
     [self.avPlayerLayer removeFromSuperlayer];
     self.avPlayer = nil;
     self.avPlayerItem = nil;
+//    [self.avplayer seekToTime:CMTimeMake(0, 1)]; // replay from the beginning
 }
 
 - (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
     if ([keyPath isEqualToString:@"status"]) {
         if (((NSNumber *)[change objectForKey:NSKeyValueChangeNewKey]).integerValue == AVPlayerItemStatusReadyToPlay) {
+            CMTime duration = self.avPlayerItem.duration;
+            CGFloat videoSecons = CMTimeGetSeconds(duration);
             [self.avPlayer play];
         } else {
             NSLog(@"Play Asset load failure");
         }
+    } else if ([keyPath isEqualToString:@"loadedTimeRanges"]) {
+        NSLog(@"Buffered %@", [change objectForKey:NSKeyValueChangeNewKey]);
     }
 }
 @end
